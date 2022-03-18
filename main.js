9af49104ce7a21b9fb1c4a71185c5fb3ed89d7d3
Santasri97 <90270303+Santasri97@users.noreply.github.com>
2022-03-18 20:34:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/2joGUB611/model.json",modelready);
+}
+function modelready() {
+    classifier.classify(gotresults);
+}
